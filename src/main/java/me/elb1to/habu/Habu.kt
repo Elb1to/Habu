cb9a937fe0c80599a4d52039d2ef645774ab1f9b
Elb1to <elbitomc@gmail.com>
2022-11-21 13:07:35 -0500

    Main class and plugin.yml

---
+++ src/main/java/me/elb1to/habu/Habu.kt
@@ -0,0 +1,18 @@
+package me.elb1to.habu
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * @author Elb1to
+ * @since 11/21/2022
+ * © 2022 Habu from FrozedClub
+ */
+class Habu : JavaPlugin() {
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
